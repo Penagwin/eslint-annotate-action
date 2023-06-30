70808c35d8b200bcb818a556c1c78b47308720eb
Paul Lang <1349689+Penagwin@users.noreply.github.com>
2023-06-30 16:42:46 -0400

    Add comments?

--- src/addSummary.ts
+++ src/addSummary.ts
@@ -1,4 +1,11 @@
-import * as core from '@actions/core'
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { context } = require('@actions/github')
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const octokit = github.getOctokit(GITHUB_TOKEN);
+ 
+const { pull_request } = context.payload;
 
 /**
  * Add to job summary
@@ -6,4 +13,10 @@ import * as core from '@actions/core'
 export default async function addSummary(summary: string): Promise<void> {
   core.summary.addRaw(summary)
   await core.summary.write()
+
+await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: summary
+  });
 }
